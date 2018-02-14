1fc0cc601603fb482d1b3c91d28748b0935e4e35
FleurTamboezer <ftamboezer@gmail.com>
2018-02-14 17:21:21 +0100

    Started writing feature tests for user sign up

---
+++ features/step_definitions/user_steps.rb
@@ -0,0 +1,31 @@
+Given("User visits the site") do
+  visit '/' # Write code here that turns the phrase above into concrete actions
+end
+
+When("User clicks on the {string} button") do |button|
+  click_link(button) # Write code here that turns the phrase above into concrete actions
+end
+
+Then("He should be redirected to {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("He fills in {string} with {string}") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("He fills in a {string} with {string}") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("He clicks {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("He sees message {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("He should be on {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
---
+++ features/user_signs_up.feature
@@ -0,0 +1,15 @@
+Feature: User can sign up
+  as a User
+  to be able to order products
+  there need to be a create account function
+
+  Scenario: User creates an account
+      Given User visits the site
+      When User clicks on the "signup" button
+      Then He should be redirected to "/signup"
+      And He fills in "name" with "Holger"
+      And He fills in a "password" with "password1"
+      And He fills in "password_confirmation" with "password1"
+      When He clicks "submit"
+      Then He sees message "signup successful"
+      And He should be on "main page"
