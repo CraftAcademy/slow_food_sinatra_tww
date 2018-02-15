20aee8cf1afc2d9d993c9bbe8f35513df4f6fd30
Oskar Brännström <oskar@quad.se>
2018-02-15 11:32:13 +0100

    Trying to unfck everything

--- features/show_menu.feature
+++ features/show_menu.feature
@@ -6,12 +6,14 @@ You want to display a Menu on a page.
 Background:
   Given "Pasta" is in "Menu"
   And "Köttbullar" is in "Menu"
+  And "Starter" is in "Menu"
 
 Scenario:
-  When the user visits the site
-  Then the user should see the "Menu"
+  When the user visits the menu page
+  Then the user should see "Menu"
+  And  show me the page
   And  the user should see "Starter"
   And  the user should see "Main Course"
   And  the user should see "Desert"
-  And user should see the "Pasta"
-  And user should see the "Köttbullar"
+  And  the user should see "Pasta"
+  And  the user should see "Köttbullar"
--- features/step_definitions/menu_steps.rb
+++ features/step_definitions/menu_steps.rb
@@ -3,31 +3,14 @@ Given("{string} is in {string}") do |product_name, menu_name|
   Product.create(name: product_name, menu: @menu)
 end
 
-When("the user visits the site") do
+When("the user visits the menu page") do
   visit "/menu"
 end
 
-Then("the user should se the {string}") do |text|
-    visit "/menu"
-  expect(page).to have_content text
-end
-
-Then("user should se the {string}") do |text|
-    visit "/menu"
-  expect(page).to have_content text
-end
-
-Then("the user should see the {string}") do |text|
-    visit "/menu"
-  expect(page).to have_content text
+Then('show me the page') do
+    save_and_open_page
 end
 
 Then("the user should see {string}") do |text|
-    visit "/menu"
-  expect(page).to have_content text
-end
-
-Then("user should see the {string}") do |text|
-    visit "/menu"
-  expect(page).to have_content text
+    expect(page).to have_content text
 end
