6c7599f2c48b6659fbee3950d69582ead4e943b8
Jade <jywfang@gmail.com>
2018-02-16 10:23:06 +0100

    fixed merge conflict

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,23 +1,6 @@
 class SlowFoodApp
-
-  get '/' do
-    erb :welcome
-  end
-
-  post '/' do
-    if params[:name].present? && params[:password] != params[:password_confirmation]
-      @message = "Passwords must match"
-    elsif params[:name].blank? || params[:password].blank? || params[:password_confirmation].blank?
-      @message = "Please enter all fields"
-    elsif params[:name].present? && params[:password].present? && params[:password_confirmation].present?
-      User.create(name: params[:name], password: params[:password])
-      @message = "signup successful"
+  get '/menu' do
+      @menu = Menu.first
+      erb :menu
     end
-    erb :welcome
-  end
-
-  get '/signup' do
-    erb :signup
-  end
-
 end
