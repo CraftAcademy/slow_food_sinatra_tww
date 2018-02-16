abc14d64ead4db2f0791643b535cbcb3215d4deb
Jade <jywfang@gmail.com>
2018-02-16 09:46:05 +0100

    made requested changes

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -4,18 +4,18 @@ Given("User visits the site") do
 When("User clicks on the {string} button") do |button|
   click_on(button)
 
-Then("He should be redirected to {string}") do |string|
-  visit string
+Then("He should be redirected to {string}") do |link|
+  visit link
 end
 
 Then("He fills in {string} with {string}") do |field, input|
-  fill_in(field, :with => input) 
+  fill_in(field, :with => input)
 end
 
-Then("He clicks {string}") do |string|
-  click_on(string)
+Then("He clicks {string}") do |button|
+  click_on(button)
 end
 
-Then("He sees message {string}") do |string|
-  page.should have_content('signup successful')
+Then("He sees message {string}") do |message|
+  page.should have_content(message)
 end
