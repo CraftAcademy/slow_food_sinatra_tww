add989decd98b40c96b0efb10656dd5618b8e5ce
Jade <jywfang@gmail.com>
2018-02-16 10:30:03 +0100

    hopefully actually fixed merge conflict

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,10 +1,11 @@
 class SlowFoodApp
-  get '/' do
-   erb :welcome
- end
 
- get '/menu' do
-   @menu = Menu.first
-   erb :menu
- end
+  get '/' do
+    erb :welcome
+  end
+
+  get '/menu' do
+    @menu = Menu.first
+    erb :menu
+  end
 end
