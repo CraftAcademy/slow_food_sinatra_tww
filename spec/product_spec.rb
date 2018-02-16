00f365df1b5dac64912a20293b29b63ef493ddd7
Holger_Tidemand <tidemand.holger@gmail.com>
2018-02-16 09:53:46 +0100

    fouth round..

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -2,11 +2,10 @@ require 'spec_helper'
 
 describe Product do
 
-it { is_expected.to have_db_column :name }
-it { is_expected.to have_db_column :description }
-it { is_expected.to have_db_column :price }
-it { is_expected.to have_db_column :catagory }
-
-it { is_expected.to belong_to :menu }
+  it { is_expected.to have_db_column :name }
+  it { is_expected.to have_db_column :description }
+  it { is_expected.to have_db_column :price }
+  it { is_expected.to have_db_column :catagory }
+  it { is_expected.to belong_to :menu }
 
 end
